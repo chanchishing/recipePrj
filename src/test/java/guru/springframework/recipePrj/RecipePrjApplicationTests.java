bce749bbc3cf2a77c291511c9380cfa50c97b0f9
chanchishing <chishing@netvigator.com>
2022-01-17 18:02:40 +0800

    1st commit

---
+++ src/test/java/guru/springframework/recipePrj/RecipePrjApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.recipePrj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipePrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
