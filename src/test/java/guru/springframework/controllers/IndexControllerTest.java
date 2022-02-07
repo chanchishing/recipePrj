ba41aac3a55293d5fb91e1dc20a99d7262e1781b
shing <chishing@netvigator.com>
2022-02-07 14:16:36 +0800

    Add ArgumentCaptor to check number of recipe object return is as expected similar to course video

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -1,12 +1,19 @@
 package guru.springframework.controllers;
 
+import guru.springframework.model.Recipe;
 import guru.springframework.service.RecipeServiceImpl;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -35,13 +42,28 @@ class IndexControllerTest {
     @Test
     void getIndexPage() {
 
-        //verify "index" is returned from indexController.getIndexPage()
-        assertEquals("index",indexController.getIndexPage(model));
+        //given
+        List<Recipe> recipes=new ArrayList<>();
+        recipes.add(new Recipe());
+        recipes.add(new Recipe());
 
-        //check recipeService.getRecipeList() is called once
+        when(recipeService.getRecipeList()).thenReturn(recipes);
+
+        ArgumentCaptor<List<Recipe>> argumentCaptor = ArgumentCaptor.forClass(List.class);
+
+        //when
+        String viewNameReturned=indexController.getIndexPage(model);
+
+        //then a) verify "index" is returned from indexController.getIndexPage()
+        assertEquals("index",viewNameReturned);
+
+        //then b) check recipeService.getRecipeList() is called once
         verify(recipeService,times(1)).getRecipeList();
 
-        //check model.addAttribute("recipes",<List>) is called once
-        verify(model,times(1)).addAttribute(eq("recipes"),anyList());
+        //then c) check model.addAttribute("recipes",<List>) is called once and 2 recipes is added
+        verify(model,times(1)).addAttribute(eq("recipes"),argumentCaptor.capture());
+        List<Recipe> listInController = argumentCaptor.getValue();
+        assertEquals(2,listInController.size());
+
     }
 }
