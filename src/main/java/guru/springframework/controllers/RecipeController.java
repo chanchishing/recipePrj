05a1b86ccfbdb7e69906a5f582b0c10dc1a5e2a2
shing <chishing@netvigator.com>
2022-02-16 14:39:57 +0800

    Initial commit for adding new recipe and getting it back to display.

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -1,18 +1,18 @@
 package guru.springframework.controllers;
 
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.model.Recipe;
 import guru.springframework.service.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
 
-import static java.lang.Long.parseLong;
-import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @Slf4j
 @Controller
@@ -33,5 +33,21 @@ public class RecipeController {
         model.addAttribute("recipe",recipe);
 
         return "/recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand=recipeService.saveRecipe(command);
+        return("redirect:/recipe/show/"+savedCommand.getId());
+
     }
 }
