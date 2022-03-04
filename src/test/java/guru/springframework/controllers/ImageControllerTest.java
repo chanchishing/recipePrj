9a8626c2ff73510b1250a3005f48eb67a9dd711f
shing <chishing@netvigator.com>
2022-03-04 16:18:33 +0800

    Initial commit of upload and display recipe image

---
+++ src/test/java/guru/springframework/controllers/ImageControllerTest.java
@@ -0,0 +1,121 @@
+package guru.springframework.controllers;
+
+import guru.springframework.commands.RecipeCommand;
+import guru.springframework.model.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+import guru.springframework.service.ImageService;
+import guru.springframework.service.RecipeService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+class ImageControllerTest {
+
+    MockMvc mockMvc;
+    private AutoCloseable closeable;
+
+    @Mock
+    RecipeService mockRecipeService;
+
+    @Mock
+    ImageService mockImageService;
+
+
+    @BeforeEach
+    void setUp() {
+        closeable = MockitoAnnotations.openMocks(this);
+        ImageController imageController = new ImageController(mockRecipeService,mockImageService);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(imageController).build();
+
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        closeable.close();
+    }
+
+    @Test
+    void loadImageUploadPage() throws Exception {
+        Long testRecipeId=1L;
+        String testRecipeIdString=String.valueOf(testRecipeId);
+        RecipeCommand recipeCommand= new RecipeCommand();
+        recipeCommand.setId(testRecipeId);
+
+        when(mockRecipeService.getRecipeCommandById(anyLong())).thenReturn(recipeCommand);
+
+        mockMvc.perform(get("/recipe/"+testRecipeIdString+"/image"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/imageUploadForm"))
+                .andExpect(model().attributeExists("recipe"))
+                .andExpect(model().attribute("recipe",is(instanceOf(RecipeCommand.class))))
+                .andExpect(model().attribute("recipe",hasProperty("id",is(testRecipeId))));
+
+        verify(mockRecipeService, times(1)).getRecipeCommandById(testRecipeId);
+    }
+
+    @Test
+    void saveImageUploaded() throws Exception {
+        Long testRecipeId=1L;
+
+        MockMultipartFile mockImageFile= new MockMultipartFile("imagefile","testing.png",
+                "image/png","Just Some Bytes".getBytes());
+
+        mockMvc.perform(multipart("/recipe/"+String.valueOf(testRecipeId)+"/image").file(mockImageFile))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location","/recipe/"+String.valueOf(testRecipeId)+"/show"))
+                .andExpect(view().name("redirect:/recipe/"+String.valueOf(testRecipeId)+"/show"));
+
+        verify(mockImageService, times(1)).saveRecipeImage(testRecipeId, mockImageFile);
+    }
+
+
+
+    @Test
+    public void renderImageFromDB() throws Exception {
+
+        //given
+        RecipeCommand command = new RecipeCommand();
+        command.setId(1L);
+
+        String s = "fake image text";
+        Byte[] bytesBoxed = new Byte[s.getBytes().length];
+
+        int i = 0;
+
+        for (byte primByte : s.getBytes()){
+            bytesBoxed[i++] = primByte;
+        }
+
+        command.setImage(bytesBoxed);
+
+        when(mockRecipeService.getRecipeCommandById(anyLong())).thenReturn(command);
+
+        //when
+        MockHttpServletResponse response = mockMvc.perform(get("/recipe/1/recipeimage"))
+                .andExpect(status().isOk())
+                .andReturn().getResponse();
+
+        byte[] responseBytes = response.getContentAsByteArray();
+
+        assertEquals(s.getBytes().length, responseBytes.length);
+    }
+}
