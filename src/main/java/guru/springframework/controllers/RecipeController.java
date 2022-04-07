ed57c53d7efee0576362a959875f668709a65a28
chanchishing <chishing@netvigator.com>
2022-04-07 12:52:29 +0800

    remove unnecessary @Transactional saveOrUpdate() method of RecipeController.java

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -55,7 +55,6 @@ public class RecipeController {
 
     }
 
-    @Transactional
     @PostMapping("recipe")
     public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult){
         if (bindingResult.hasErrors()) {
